1cb31204c81d6ab50f47f1fbcc60f6d38469cb37
GiveMe-A-Name <a455300764b@hotmail.com>
2023-04-19 17:02:17 +0800

    test01

--- plugins.js
+++ plugins.js
@@ -12,8 +12,14 @@ module.exports = class MyPlugin {
         () => {
           const stats = compilation.getStats().toJson({
             all: false,
+            assets: true,
+            chunks: true,
             chunkGroups: true,
             entrypoints: true,
+            hash: true,
+            ids: true,
+            outputPath: true,
+            publicPath: true,
           });
 
           const filepath = path.resolve(__dirname, "./status.json");
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import(/* webpackChunkName: "user/[id]/page" */ "./Hello1").then((module) => {
   module.default();
 });
-import(/* webpackChunkName: "redirect/page"" */ "./Hello2").then((module) => {
+import(/* webpackChunkName: "redirect/page" */ "./Hello2").then((module) => {
   module.default();
 });
